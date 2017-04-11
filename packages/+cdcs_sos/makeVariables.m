701a5e709cc41cbdd6c96f970beb5ba34e13cdba
Giovanni Fantuzzi <giofantuzzi@hotmail.it>
2017-04-11 16:24:20 +0100

    Changed initial point to match SCS

--- packages/+cdcs_sos/makeVariables.m
+++ packages/+cdcs_sos/makeVariables.m
@@ -7,16 +7,17 @@ function [X,Y,Z,others] = makeVariables(K,initVars,opts)
 
 if isempty(initVars)
     %initialize vectorized versions of all variables
-    X.x  = zeros(opts.n,1);                % cone variables 
-    X.y  = zeros(opts.m,1);                % free variables
-    X.tau = 1;
+    X.x   = zeros(opts.n,1);                % cone variables 
+    X.y   = zeros(opts.m,1);                % free variables
+    X.tau = sqrt(opts.n+opts.m+1);          % set as sqrt(n+m+1) in SCS?
     
-    Y.x  = zeros(opts.n,1);                % cone variables 
-    Y.y  = zeros(opts.m,1);                % free variables
-    Y.tau = 1;
+    
+    Y.x   = zeros(opts.n,1);                % cone variables 
+    Y.y   = zeros(opts.m,1);                % free variables
+    Y.tau = sqrt(opts.n+opts.m+1);
 
-    Z.x  = zeros(opts.n,1);                % similar to Y
-    Z.y  = zeros(opts.m,1);                % 
+    Z.x     = zeros(opts.n,1);                % similar to Y
+    Z.y     = zeros(opts.m,1);                % 
     Z.kappa = 0;
     
     others.X    = cdcs_utils.makeConeVariables(K);% cone variables: matrix version
